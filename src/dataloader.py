316d5f8d60f585eddddd437cce4b0b990da08362
Ira Kones <irakones@gmail.coml>
2018-01-06 13:45:43 -0800

    dataloader quantizes outcomes

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,7 +1,8 @@
 from collections import deque
 import pandas as pd
+import numpy as np
 
-def get_data(path, interval_length=1, predict_length=10, window_length=100):
+def get_data(path, interval_length, predict_length):
     """ interval_length: number of seconds to aggregate together to regularize
                          sample frequency
         window_length: length to be used to rolling window generator
@@ -17,10 +18,10 @@ def get_data(path, interval_length=1, predict_length=10, window_length=100):
     agg_df.price = agg_df.price.fillna(method='ffill')
     agg_df['change'] = agg_df.price.pct_change()
 
-    outcomes  = agg_df.price.pct_change(periods=predict_length).as_matrix()[1:]
+    outcomes  = agg_df.price.pct_change(periods=predict_length).as_matrix()[11:]
     features = agg_df[['volume', 'price', 'change']].as_matrix()[1:]
 
-    return window_gen(features, outcomes, window_length, predict_length)
+    return features, outcomes
 
 def discretize(df, interval_length):
     """ interval_length = number of seconds to aggregate in """
@@ -46,12 +47,14 @@ def window_gen(features, outcomes, window_length, predict_length):
     last_price = cur[-1][1] 
 
     for i in range(window_length, len(features) - predict_length):
-        yield {
-            'x': cur,
-            'y': outcomes[i + predict_length],
-            'last_price':  last_price
-        }
+        yield [cur, outcomes[i], last_price]
         cur.popleft()
         cur.append(features[i, [0, 2]])
         last_price = features[i, 1]
 
+def main(path='data/data.csv', predict_length=10, interval_length=1, window_length=100):
+    features, outcomes = get_data(path, interval_length, predict_length)
+    return window_gen(features, quantize(outcomes), window_length, predict_length)
+
+def quantize(items, amin=-0.01, amax=0.01, step=1e-5):
+    return np.digitize(np.clip(items, amin, amax), np.arange(amin, amax, step))
