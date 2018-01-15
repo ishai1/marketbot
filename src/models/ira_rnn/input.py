959019a8148609c9c8e0d7128a1684e277f740d2
Ira Kones <irakones@gmail.coml>
2018-01-14 17:16:00 -0800

    models subdirectories

--- src/models/ira_rnn/input.py
+++ src/models/ira_rnn/input.py
@@ -7,3 +7,4 @@ def train_input_fn(path, window, horizon):
     data = tf.convert_to_tensor(data)
     targets = data[horizon + window - 1 :, 1] / data[window - 1 : -horizon, 1] - 1
     features = tf.contrib.signal.frame(data, window, 1, axis=0)[:-horizon]
+    return features, targets
