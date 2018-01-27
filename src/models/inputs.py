Fix normalization and refractor

src/models/inputs.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from .utils import normalize
 import numpy as np
 import os
 
@@ -69,31 +70,3 @@ def _read_csv_to_tensor(path):
     data = np.genfromtxt(path, delimiter=',')[2:, 1:]
     return tf.convert_to_tensor(data, dtype=tf.float32)
 
-
-def normalize(X, name, mode):
-    """
-    Apply normalization across axis 0 for variable X.
-    Normalization params are collected from scope = name.
-    """
-    with tf.variable_scope(os.path.join('normalize', name)):
-        if mode == ModeKeys.TRAIN:
-            mu, std = calc_mu_std(X)
-        else:
-            shape = (X.shape[1],)
-            mu = tf.get_variable('mean', shape=shape, trainable=False)
-            std = tf.get_variable('std', shape=shape, trainable=False)
-    return tf.div(X - mu, std + 1e-8)
-
-
-def calc_mu_std(X):
-    """
-    Calculate mean and standard devation and store for later use.
-    Use assign instead of initalize in-case the mu / std already exist.
-    """
-    mu0, var = tf.nn.moments(X, axes=(0))
-    shape = (mu0.shape[0], )
-    mu = tf.get_variable('mean', shape=shape, trainable=False)
-    mu.assign(mu0)
-    std = tf.get_variable('std', shape=shape, trainable=False)
-    std.assign(tf.sqrt(var))
-    return mu, std

src/models/lstm2.py
@@ -2,7 +2,7 @@
 model definition for LSTM
 """
 import tensorflow as tf
-
+from .utils import denormalize
 from .inputs import _input_fn_wrapper
 
 ModeKeys = tf.estimator.ModeKeys
@@ -70,11 +70,8 @@ def _rnn_model_fn(features, labels, mode, params):
     optimizer = params['optimizer'](learning_rate=params['learning_rate'])
     train_op = optimizer.minimize(loss=loss + reg_loss, global_step=tf.train.get_global_step())
 
-    with tf.variable_scope('normalize/targets', reuse=True):
-        std_coeffs = tf.get_variable('std')
-        mu_coeffs = tf.get_variable('mean')
-    x = tf.squeeze(predictions) * std_coeffs + mu_coeffs
-    y = tf.squeeze(prices) * std_coeffs + mu_coeffs
+    x = denormalize(predictions, 'targets')
+    y = denormalize(prices, 'targets')
     if mode == ModeKeys.EVAL:
         pf_over_y = tf.div(y[-1], y)
     else: