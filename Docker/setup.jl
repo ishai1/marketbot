acea1af339bf0fe245b59d92655c8055461c93ec
Ishai <ishaikones@gmail.com>
2018-10-01 15:20:37 -0700

    remove docker

--- Docker/setup.jl
+++
@@ -1,9 +0,0 @@
-using Pkg # for 0.7+
-using Logging
-SetLevel
-Pkg.add("PyCall")
-Pkg.add(PackageSpec("https://github.com/malmaud/TensorFlow.jl.git") )
-# Pkg.build("TensorFlow")
-Pkg.add("IJulia")
-using TensorFlow
-using IJulia
